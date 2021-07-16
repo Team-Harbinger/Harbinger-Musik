e98287ac28d5a6bed019a435a1ed80ebf9180884
lchua2314 <lchua2314@gmail.com>
2021-07-15 23:25:30 -0700

    Initializing file system

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import MainPage from './modules/MainPage.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Harbinger Music
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <MainPage>
+      </MainPage>
   );
 }
 
---
+++ src/modules/MainPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const MainPage = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Harbinger Music
+        </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+        </a>
+            </header>
+        </div>
+    );
+}
+
+export default MainPage;
