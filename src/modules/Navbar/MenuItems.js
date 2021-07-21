7492d62a0105927cf4fe59d96eef3cff7fc81fc5
roundband5 <30790329+roundband5@users.noreply.github.com>
2021-07-20 17:38:53 -0700

    Added Navbar

---
+++ src/modules/Navbar/MenuItems.js
@@ -0,0 +1,12 @@
+export const MenuItems= [
+    {
+        title: 'Home',
+        url:'#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About',
+        url:'#',
+        cName: 'nav-links'
+    }
+]
