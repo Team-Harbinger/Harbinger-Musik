048b92891134a643fae7edc151f4d12f49bdcd6d
leonzh2k <leonzh2k@gmail.com>
2021-07-21 19:55:53 -0700

    add basic (unclickable) genre selection section of home page

--- src/modules/Home/Home.js
+++ src/modules/Home/Home.js
@@ -1,23 +1,59 @@
 import React from 'react';
-import AppLogo from '../../assets/applogo.svg';
+import {useState} from 'react';
+import './Home.css';
 
 function Home() {
+  const API_KEY = process.env.REACT_APP_NAPSTER_API_KEY;
+  const [genreData, setGenreData] = useState([]);
+  if (!genreData.length) {
+    console.log(genreData)
+    let genres = [];
+    //fetch genre images.
+    fetch('https://api.napster.com/v2.2/genres?apikey=' + API_KEY)
+    .then(function(response) {
+      // Successful response :)
+      console.log("success");
+      return response.json(); 
+    })
+    .then(function(response) {
+      console.log(response)
+      response.genres.forEach(genre => {
+        //don't need to api call the image?
+        genres.push({
+          genreName: genre.name,
+          genreImageSrc: "https://api.napster.com/imageserver/images/" + genre.id + "/240x160.jpg"
+        })
+      })
+      console.log(genres);
+      setGenreData(genres);
+    })
+    .catch(function(err) {
+      // Error :(
+        console.log("Error: unable to fetch genre data");
+    });
+  }
+  let genreDOMEles = [];
+  genreData.forEach(genre => {
+    genreDOMEles.push(
+      <div className="genre" >
+        <img src={genre.genreImageSrc} alt={"Image Representing " + genre.genreName} className="genre-image" />
+        <span className="genre-image-description">{genre.genreName}</span>
+      </div>
+    )
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={AppLogo} className="App-logo" alt="Harbinger Musik Logo" />
-        <p>
-          Harbinger Music
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Home">
+      <div className="flex-column-container">
+        {/* place navbar here */}
+        <div id="discover-genres" className="flex-column-container">
+          <h2>Discover Music</h2>
+          <div id="genre-list" className="flex-row-container">
+            {genreDOMEles}
+          </div>
+        </div>
+        
+      </div>
+      
     </div>
   );
 }
