5709d078bd8adc1ed46ac8d51cbea66d85d9408f
leonzh2k <leonzh2k@gmail.com>
2021-08-03 16:34:06 -0700

    Added ability to search via pressing Enter key

--- src/modules/Searchbar/Searchbar.js
+++ src/modules/Searchbar/Searchbar.js
@@ -4,22 +4,37 @@ import './Searchbar.css';
 import SearchIcon from '../../assets/searchicon.svg';
 
 function Searchbar() {
+  /**
+  * Holds the user's input. Updates every time the input changes 
+  * and the input element's value is set to this (controlled component)
+  */ 
   const [searchString, setSearchString] = useState("");
 
   function handleInputChange(e) {
     setSearchString(e.target.value);
   }
 
+  // only runs when the search icon is clicked.
   function validateInput(e) {
-    if (searchString === "") {
-      console.log("nothing in searchbar, don't search");
-      e.preventDefault();
+    // check whether event was a keypress or a click. 
+    if (e.key === undefined) {
+      // we don't want user to submit empty string
+      if (searchString === "") {
+        // console.log("nothing in searchbar, don't search");
+        e.preventDefault();
+      }
+    }
+    else {
+      if (e.key === "Enter" && searchString !== "") {
+        // this method of changing the page seems to work with router
+        window.location.href = "/search?query=" + searchString + "&type=track";
+      }
     }
   }
 
   return (
     <div className="search-container">
-      <input type="text" placeholder="" name="search" onChange={handleInputChange} value={searchString} />
+      <input type="text" placeholder="" name="search" onChange={handleInputChange} onKeyPress={validateInput} value={searchString} />
         {
           /**  
           * In this case the href doesn't have to match the Router path exactly.
