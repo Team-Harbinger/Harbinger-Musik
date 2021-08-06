cda7716367ae0316710bfe972455c1343ad12855
roundband5 <30790329+roundband5@users.noreply.github.com>
2021-08-05 17:36:13 -0700

    Fixed merge
    fixed merge errors

--- src/modules/Genre/Genre.js
+++ src/modules/Genre/Genre.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { useState } from 'react';
 import './Genre.css';
 import PlayButton from '../PlayButton/PlayButton';
+
 function stripHtml(html) {
   let tmp = document.createElement("DIV");
   tmp.innerHTML = html;
   return tmp.textContent || tmp.innerText || "";
 }
-function Genre() {
+
+function Genre(props) {
 
   const API_KEY = process.env.REACT_APP_NAPSTER_API_KEY;
   const [genreDetailsData, setGenreDetailsData] = useState([]);
@@ -15,12 +17,8 @@ function Genre() {
 
   // To get Genre Description:
   // Pass in genre ID -> genre API -> (genre) name, description
-
-  // Hardcoded for now
-  let genreID = 'g.115';
-  // g.115- Pop
-  // g.5 - Rock
-  // g.33 - Alternative
+  
+  let genreID = props.location.pathname.split("/")[2];
 
   // Genre Description
   if (!genreDetailsData.length) {
@@ -66,12 +64,13 @@ function Genre() {
             songImageSrc: "https://api.napster.com/imageserver/v2/albums/" + track.albumId + "/images/500x500.jpg",
             songName: track.name,
             artistName: track.artistName,
+            trackID: track.id,
             trackIndex: count,
             trackPreviewSrc: track.previewURL
           })
           count++;
         })
-        console.log(songList);
+        console.log("songlist: :", songList);
         setSongListData(songList);
 
       })
@@ -120,8 +119,10 @@ function Genre() {
             </div>
           </div>
         </div>
-        <div className="song-name list-col">
-          {track.trackIndex + '. ' + track.songName}
+        <div className="song-name col">
+          <span>{track.trackIndex + '. '}</span>
+          <a href={"/song/" + track.trackID}>{track.songName}
+          </a>
           <div className="artist-name list-col">
             {track.artistName}
           </div>
@@ -134,7 +135,7 @@ function Genre() {
     // Change to ids
     <div className="Song">
       <h2 className="genre-header">{genreNameDOMElement}</h2>
-      <div className="holder flex-row-container">        
+      <div className="holder flex-row-item">        
           <div className="song flex-row-item">
             {genreDetailsDOMElement}
         </div>
