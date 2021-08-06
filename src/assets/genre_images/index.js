d29a50576be10d6fb7fd7c3c1f094c05ec25394a
leonzh2k <leonzh2k@gmail.com>
2021-08-05 17:40:24 -0700

    changed genre images on Homepage

---
+++ src/assets/genre_images/index.js
@@ -0,0 +1,49 @@
+import PopImage from './napster-pop-image.jpg';
+import RockImage from './napster-rock-image.jpg';
+import AlternativeImage from './napster-alternative-image.jpg';
+import BluesImage from './napster-blues-image.jpg';
+import ChildrenImage from './napster-children-image.jpg';
+import ChristianImage from './napster-christian-image.jpg';
+import ClassicalImage from './napster-classical-image.jpg';
+import ComedyImage from './napster-comedy-image.jpg';
+import CountryImage from './napster-country-image.jpg';
+import ElectronicImage from './napster-electronic-image.jpg';
+import FolkImage from './napster-folk-image.jpg';
+import JazzImage from './napster-jazz-image.jpg';
+import LatinImage from './napster-latin-image.jpg';
+import MetalImage from './napster-metal-image.jpg';
+import NewAgeImage from './napster-new-age-image.jpg';
+import OldiesImage from './napster-oldies-image.jpg';
+import RapImage from './napster-rap-image.jpg';
+import ReggaeImage from './napster-reggae-image.jpg';
+import RnBImage from './napster-rnb-image.jpg';
+import SoundtracksImage from './napster-soundtracks-image.jpg';
+import VocalImage from './napster-vocal-image.jpg';
+import WorldImage from './napster-world-image.jpg';
+
+// values of the keys hold the filepath to the images as a string.
+const genreImages = {
+    "Pop": PopImage,
+    "Rock": RockImage,
+    "Alternative": AlternativeImage,
+    "Blues": BluesImage,
+    "Children": ChildrenImage,
+    "Christian/Gospel": ChristianImage,
+    "Classical": ClassicalImage,
+    "Comedy/Spoken Word": ComedyImage,
+    "Country": CountryImage,
+    "Electronic": ElectronicImage,
+    "Folk": FolkImage,
+    "Jazz": JazzImage,
+    "Latin": LatinImage,
+    "Metal": MetalImage,
+    "New Age": NewAgeImage,
+    "Oldies": OldiesImage,
+    "Rap/Hip-Hop": RapImage,
+    "Reggae": ReggaeImage,
+    "Soul/R&B": RnBImage,
+    "Soundtracks": SoundtracksImage,
+    "Vocal/Easy Listening": VocalImage,
+    "World": WorldImage,
+}
+export default genreImages;
