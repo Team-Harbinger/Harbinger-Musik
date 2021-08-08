b6a748b50fed140e5ab2ce5acbba5e4f4a5379f6
lchua2314 <lchua2314@gmail.com>
2021-08-07 18:34:51 -0700

    Made navbar fixed at the top of page when scrolling

--- src/modules/Navbar/Navbar.js
+++ src/modules/Navbar/Navbar.js
@@ -6,23 +6,26 @@ import Searchbar from '../Searchbar/Searchbar.js';
 
 function Navbar() {
   return (
-    <nav className="NavbarItems">
-      <ul className='nav-menu'>
-        {MenuItems.map((item, index) => {
-          return (
-            <li key={index}>
-              <a className={item.cName} href={item.url}>
-                {item.title}
-              </a>
-            </li>
-          )
-        })}
-      </ul>
+    <div>
+      <nav className="NavbarItems">
+        <ul className='nav-menu'>
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href={item.url}>
+                  {item.title}
+                </a>
+              </li>
+            )
+          })}
+        </ul>
 
-      <img src={AppLogo} className="navbar-logo" alt="Harbinger Musik Logo" width="70" height="70" />
-      
-      <Searchbar />
-    </nav>
+        <img src={AppLogo} className="navbar-logo" alt="Harbinger Musik Logo" width="70" height="70" />
+
+        <Searchbar />
+      </nav>
+      <div className="navbar-margin">Margin</div>
+    </div>
   )
 }
 
