5c0405be0527ee62bbf97535c7ded4eb4c84596d
leonzh2k <leonzh2k@gmail.com>
2021-08-20 15:31:31 -0700

    removed basename attribute from Router component for testing GH pages

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router basename='/Harbinger-Musik'>
+    <Router>
       <Navbar />
       <Switch>
         <Route path='/' exact component={Home} />
