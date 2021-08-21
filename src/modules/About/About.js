b2f46a4332cd5d10837cb68cea05478673ee519d
leonzh2k <leonzh2k@gmail.com>
2021-08-21 13:42:11 -0700

    Removed placholder text from About page

--- src/modules/About/About.js
+++ src/modules/About/About.js
@@ -19,15 +19,6 @@ function About() {
         as genres, playlists, song names, and playable song previews.
         <br /><br />
         <a className={`${styles["About-link"]}`} target="_blank" rel="noopener noreferrer" href="https://github.com/Team-Harbinger/Harbinger-Musik">GitHub Link</a>
-        <br /><br />
-        Placeholder text, remove later: 
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        <br /><br />
-        Placeholder text, remove later: 
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        <br /><br />
-        Placeholder text, remove later: 
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </div>
     </div>
   );
