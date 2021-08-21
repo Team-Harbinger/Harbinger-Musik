25344e9e2b8e0994d9346f8b5abd0c4a9226db15
leonzh2k <leonzh2k@gmail.com>
2021-08-21 13:38:29 -0700

    Changed one api call to use https instead of http

--- src/modules/Song/asyncFetchSongPageData.js
+++ src/modules/Song/asyncFetchSongPageData.js
@@ -1,6 +1,6 @@
 // asynchronously fetches track data
 export async function asyncFetchTrackData(albumId, API_KEY) {
-  const response = await fetch(`http://api.napster.com/v2.2/tracks/${albumId}?apikey=${API_KEY}`);
+  const response = await fetch(`https://api.napster.com/v2.2/tracks/${albumId}?apikey=${API_KEY}`);
   return response.json();
 }
 
